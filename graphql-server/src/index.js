6dd0a0ae4e8e235d07553a889f5492976c1ef76c
Elias D <smooth.criminal5698@gmail.com>
2025-03-15 23:15:01 +0000

    added cors, caching and compression

--- graphql-server/src/index.js
+++ graphql-server/src/index.js
@@ -1,48 +1,78 @@
 const { ApolloServer } = require('apollo-server');
-const { PrismaClient } = require('@prisma/client')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const User = require('./resolvers/User')
-const Post = require('./resolvers/Post')
-const Comment = require('./resolvers/Comment')
-const Tag = require('./resolvers/Tag')
-
-
-const resolvers = {
-    Query,
-    Mutation,
-    User,
-    Post,
-    Comment,
-    Tag
-  }
-  
-
+const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 const path = require('path');
-const prisma = new PrismaClient()
+const compression = require('compression');
+const http = require('http');
 
+const prisma = new PrismaClient();
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Post = require('./resolvers/Post');
+const Comment = require('./resolvers/Comment');
+const Tag = require('./resolvers/Tag');
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Post,
+  Comment,
+  Tag
+};
+
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+};
+
+// First, create the Apollo Server
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, 'schema.graphql'),
-    'utf8'
-  ),
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
-  context: ({ req }) => {  //Checks if the request has an Authorization header.
-    // If yes, it calls getUserId(req) to extract userId from the JWT.
-    // If no, userId is set to null (meaning the user is not authenticated).
+  context: ({ req }) => {
     return {
       ...req,
       prisma,
     };
-  }
+  },
+  cors: corsOptions,
+  compression: true,
+  plugins: [
+    {
+      requestDidStart(requestContext) {
+        requestContext.response.http.headers.set('Cache-Control', 'public, max-age=3600');
+        requestContext.response.http.headers.set('ETag', 'some-version-hash');
+        return {
+          didResolveOperation(context) {
+            const lastModified = new Date().toUTCString();
+            requestContext.response.http.headers.set('Last-Modified', lastModified);
+          },
+        };
+      },
+    },
+  ],
 });
 
-
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+// Create an HTTP server that applies compression
+server.createGraphQLServerOptions = async (req, res) => {
+  const options = await server.graphQLServerOptions({ req, res });
   
+  // Enable compression for this request
+  const compressMiddleware = compression();
+  
+  // Use the middleware on this request
+  return new Promise((resolve) => {
+    compressMiddleware(req, res, () => {
+      resolve(options);
+    });
+  });
+};
 
+// Start the server
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url} with compression enabled`);
+});
